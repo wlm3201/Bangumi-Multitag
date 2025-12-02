d06d42dbe5b370232ae699dc196bdf56eba11444
wlm3201 <wlm3201@qq.com>
2025-12-03 01:42:20 +0800

    1

--- src/stores/db.js
+++ src/stores/db.js
@@ -14,10 +14,8 @@ async function reload() {
 }
 async function load() {
   let os = localStorage.getItem(p)
-  console.log('os', os)
   if (!os) return await loadNew()
   let s = await net.getSize(p)
-  console.log('s', s)
   if (+os == s) return await loadOld()
   let ndb = await loadNew()
   let odb = await loadOld()
@@ -26,12 +24,11 @@ async function load() {
 }
 async function loadNew() {
   let r = await fetch(p)
+  localStorage.setItem(p, r.headers.get('content-length'))
   let b = await r.blob()
   let o = await unzip(b)
   let f = Object.values(o)[0]
   let ndb = await sqlite3(f)
-  console.log('b', b.size)
-  localStorage.setItem(p, b.size)
   init(ndb)
   save(ndb)
   return ndb
